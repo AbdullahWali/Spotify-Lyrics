a9257a6eeb063ed11f6b21c5b63d925abdba23a7
abdullahwali <aboudwali@gmail.com>
2017-10-08 00:36:20 +0300

    Version 1

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ let port = 3000;
 module.exports = {
 	port: port,
     redirectUri : 'http://localhost:'+ port + '/callback',
-    clientId : '---------YOUR SPOTIFY CLIENT ID ------------',
-    clientSecret : '------YOUR SPOTIFY CLIENT SECRET-----------'
+    clientId : '229282f4028d4eeb8f342fc0f317d609',
+    clientSecret : 'b78c52dc1baf43fca111b3de31989014'
 };
